974b3a41573b18d59cc179c28379c76366c6849e
noble-shake <coppgabliw@gmail.com>
2024-05-30 21:55:40 +0900

    2024-05-30 Move / Jump

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerManager: MonoBehaviour
 {
@@ -20,16 +22,22 @@ public class PlayerManager: MonoBehaviour
     public static PlayerManager instance;
 
     [Header("External Setup")]
-    Camera mainCam;
+    [SerializeField] Camera mainCam;
+    [SerializeField] GameObject ChracterPortrait;
 
     [Header("Player Control")]
-    [SerializeField] bool isLeftChar = true;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float jumpForce;
+    [SerializeField] bool isMovable;
+    [SerializeField] bool isFirstChar;
     [SerializeField] float exCool;
     [SerializeField] float exCurCool;
+    [SerializeField] bool isAiming;
 
     [Header("Player Object")]
     [SerializeField] PlayerScript Player1Object;
     [SerializeField] PlayerScript Player2Object;
+    [SerializeField] PlayerScript TargetObject;
     [SerializeField] Rigidbody rigid;
 
     private void Awake()
@@ -46,21 +54,32 @@ public class PlayerManager: MonoBehaviour
 
     // Stage Initialize
     public void StageInit()
-    { 
+    {
         // position setup
+        Player1Object.GetComponent<PlayerScript>().playerCam.SetActive(true);
+        Player2Object.GetComponent<PlayerScript>().playerCam.SetActive(false);
+        Player1Object.GetComponent<Rigidbody>().mass = 1f;
+        Player2Object.GetComponent<Rigidbody>().mass = 1000f;
+        TargetObject = Player1Object;
+        rigid = Player1Object.GetComponent<Rigidbody>();
     }
 
     void Start()
     {
-        mainCam = Camera.main;
-        rigid = Player1Object.GetComponent<Rigidbody>();
+        StageInit();
+        
     }
 
 
     void Update()
     {
         CharacterExchange();
+        CharacterMove();
+        CharacterJump();
+        CharacterAim();
     }
+
+
 
     private void CharacterExchange()
     {
@@ -69,26 +88,91 @@ public class PlayerManager: MonoBehaviour
         if (exCurCool < 0f)
         { 
             exCurCool = 0f;
+            // isMovable = true;
         }
 
         if (exCurCool > 0f) return;
+        if (isAiming) return;
 
         if (Input.GetKeyDown(KeyCode.C))
         {
-            isLeftChar = !isLeftChar;
+            isFirstChar = !isFirstChar;
             exCurCool = exCool;
+            // isMovable = false;
+
+            // Camera Change
+            // Character Controller Change
+            if (isFirstChar)
+            {
+                Player1Object.GetComponent<PlayerScript>().playerCam.SetActive(false);
+                Player1Object.GetComponent<PlayerScript>().CharacterSelect = false; // isSelect On/Off
+                Player2Object.GetComponent<PlayerScript>().playerCam.SetActive(true);
+                Player2Object.GetComponent<PlayerScript>().CharacterSelect = true;
+                Player1Object.GetComponent<Rigidbody>().mass = 1000f;
+                Player2Object.GetComponent<Rigidbody>().mass = 1f;
+                TargetObject = Player2Object;
+                rigid = Player2Object.GetComponent<Rigidbody>();
+            }
+            else
+            {
+                Player1Object.GetComponent<PlayerScript>().playerCam.SetActive(true);
+                Player1Object.GetComponent<PlayerScript>().CharacterSelect = false;
+                Player2Object.GetComponent<PlayerScript>().playerCam.SetActive(false);
+                Player2Object.GetComponent<PlayerScript>().CharacterSelect = true;
+                Player1Object.GetComponent<Rigidbody>().mass = 1f;
+                Player2Object.GetComponent<Rigidbody>().mass = 1000f;
+                TargetObject = Player1Object;
+                rigid = Player1Object.GetComponent<Rigidbody>();
+            }
         }
 
-        // Camera Change
-        // Character Controller Change
-        if (isLeftChar)
+
+
+    }
+
+    private void CharacterMove() {
+        if (isAiming) return;
+        // if (!isMovable) return;
+
+        float vert = Input.GetAxis("Vertical");
+        float hori = Input.GetAxis("Horizontal");
+
+        Vector3 moveDir = Vector3.zero;
+        moveDir.x = hori * moveSpeed;
+        moveDir.y = rigid.velocity.y;
+        rigid.velocity = TargetObject.transform.rotation * moveDir;
+    }
+
+    private void CharacterJump() {
+
+        if (Input.GetKeyDown(KeyCode.X) && TargetObject.GroundCheck)
         {
-
+            rigid.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
-        else
-        { 
-            
-        }
+    }
 
+    private void CharacterAim()
+    {
+        //// exchange icon cool time change
+        //exCurCool -= Time.deltaTime;
+        //if (exCurCool < 0f)
+        //{
+        //    exCurCool = 0f;
+        //    // isMovable = true;
+        //}
+
+        //if (exCurCool > 0f) return;
+
+        if (Input.GetKeyDown(KeyCode.Z) && TargetObject.GroundCheck && !isAiming)
+        {
+            TargetObject.GetComponent<PlayerScript>().playerAimCam.SetActive(true);
+            isAiming = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Z) && isAiming)
+        {
+            TargetObject.GetComponent<PlayerScript>().playerCam.SetActive(true);
+            isAiming = false;
+        }
+        
     }
 }
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,18 +7,59 @@ public class PlayerScript : MonoBehaviour
     [Header("Character Physics")]
     [SerializeField] Rigidbody rigid;
     [SerializeField] CapsuleCollider coll;
+    [SerializeField] GameObject PlayerCam;  // Cinemachine Object
+    [SerializeField] GameObject PlayerAimCam;  // Cinemachine Object
+    [SerializeField]
+    public GameObject playerCam
+    { 
+        get {
+            PlayerAimCam.SetActive(false);
+            return PlayerCam; 
+        }
+        set { PlayerCam = value; }
+    }
+
+    public GameObject playerAimCam
+    {
+        get {
+            PlayerCam.SetActive(false);
+            return PlayerAimCam; 
+        }
+        set { PlayerCam = value; }
+    }
+
+    [SerializeField] bool isGround;
+    [SerializeField] public bool GroundCheck
+    { get { return isGround; } set { isGround = value; } }
+
+    [Header("Character Control")]
+    [SerializeField] bool isSelect;
+    public bool CharacterSelect
+    { get { return isSelect; } set { isSelect = value; } }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerAimCam.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        JumpCheck();
     }
+
+    private void JumpCheck() {
+        if (Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, 1.1f, LayerMask.GetMask("Ground")))
+        {
+            isGround = true;
+        }
+        else
+        {
+            isGround = false;
+        }
+    }
+
 
 
 }
