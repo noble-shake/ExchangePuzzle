4ab8d450b8a20ee356a7c39640011929de32b9f3
noble-shake <coppgabliw@gmail.com>
2024-07-02 17:42:47 +0900

    2024-07-02

---
+++ Assets/Scripts/EffectScript/AimEffectScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimEffectScript : MonoBehaviour
+{
+    [SerializeField] Sprite AimEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AlphaChange(bool _active)
+    { 
+    
+    
+    }
+
+
+}
